d7a53c7f25406655b4f3c35e131b72108bc3caf6
shahibur-shanto <shahibur407@gmail.com>
2023-07-30 00:56:19 +0600

    details details

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -21,90 +21,64 @@ export const BookDetail = () => {
   return (
     <>
       <Navbar />
-      <div class="min-h-screen flex items-center justify-center px-4">
-        <div class="max-w-4xl  bg-white w-full rounded-lg shadow-xl">
-          <div class="p-4 border-b">
-            <h2 class="text-2xl ">Applicant Information</h2>
-            <p class="text-sm text-gray-500">
-              Personal details and application.
-            </p>
+      <div className="min-h-screen flex items-center justify-center px-4">
+        <div className="max-w-4xl  bg-white w-full rounded-lg shadow-xl">
+          <div className="p-4 border-b">
+            <h2 className="text-2xl ">Books Details</h2>
           </div>
           <div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p class="text-gray-600">Full name</p>
-              <p>Jane Doe</p>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Title</p>
+              <p>{title}</p>
             </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p class="text-gray-600">Application for</p>
-              <p>Product Manager</p>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Author</p>
+              <p>{author}</p>
             </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p class="text-gray-600">Email Address</p>
-              <p>Janedoe@gmail.com</p>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Genre</p>
+              <p>{genre}</p>
             </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p class="text-gray-600">Salary</p>
-              <p>$ 12000</p>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Publication Date</p>
+              <p>{publication_date}</p>
             </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p class="text-gray-600">About</p>
-              <p>
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-                incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                proident. Irure nostrud pariatur mollit ad adipisicing
-                reprehenderit deserunt qui eu.
-              </p>
-            </div>
-            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4">
-              <p class="text-gray-600">Attachments</p>
-              <div class="space-y-2">
-                <div class="border-2 flex items-center p-2 rounded justify-between space-x-2">
-                  <div class="space-x-2 truncate">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="fill-current inline text-gray-500"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M17 5v12c0 2.757-2.243 5-5 5s-5-2.243-5-5v-12c0-1.654 1.346-3 3-3s3 1.346 3 3v9c0 .551-.449 1-1 1s-1-.449-1-1v-8h-2v8c0 1.657 1.343 3 3 3s3-1.343 3-3v-9c0-2.761-2.239-5-5-5s-5 2.239-5 5v12c0 3.866 3.134 7 7 7s7-3.134 7-7v-12h-2z" />
-                    </svg>
-                    <span>resume_for_manager.pdf</span>
-                  </div>
-                  <a href="#" class="text-purple-700 hover:underline">
-                    Download
-                  </a>
-                </div>
 
-                <div class="border-2 flex items-center p-2 rounded justify-between space-x-2">
-                  <div class="space-x-2 truncate">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="fill-current inline text-gray-500"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M17 5v12c0 2.757-2.243 5-5 5s-5-2.243-5-5v-12c0-1.654 1.346-3 3-3s3 1.346 3 3v9c0 .551-.449 1-1 1s-1-.449-1-1v-8h-2v8c0 1.657 1.343 3 3 3s3-1.343 3-3v-9c0-2.761-2.239-5-5-5s-5 2.239-5 5v12c0 3.866 3.134 7 7 7s7-3.134 7-7v-12h-2z" />
-                    </svg>
-                    <span>resume_for_manager.pdf</span>
-                  </div>
-                  <a href="#" class="text-purple-700 hover:underline">
-                    Download
-                  </a>
-                </div>
-              </div>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Input your Review"
+              />
+
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Submit Review
+              </button>
+            </div>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Edit
+              </button>
+
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Delete
+              </button>
+            </div>
+            <div className="md:grid md:grid-cols-1 m-2 py-2 px-4 rounded bg-cyan-500 hover:bg-cyan-600">
+              Reviews:
             </div>
           </div>
         </div>
-        <a
-          href="https://www.buymeacoffee.com/danimai"
-          target="_blank"
-          class="bg-purple-600 p-2 rounded-lg text-white fixed right-0 bottom-0"
-        >
-          Support me
-        </a>
       </div>
 
       <Footer />
