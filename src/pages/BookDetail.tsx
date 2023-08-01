c91dd0da12f10bdca2d58ee0ade9d8375a54bc0d
shahibur-shanto <shahibur407@gmail.com>
2023-08-01 22:16:29 +0600

    edit

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -33,65 +33,67 @@ export const BookDetail = () => {
           <div className="p-4 border-b">
             <h2 className="text-2xl ">Books Details</h2>
           </div>
-          <div>
-            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p className="text-gray-600">Title</p>
-              <p>{title}</p>
-            </div>
-            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p className="text-gray-600">Author</p>
-              <p>{author}</p>
-            </div>
-            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p className="text-gray-600">Genre</p>
-              <p>{genre}</p>
-            </div>
-            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <p className="text-gray-600">Publication Date</p>
-              <p>{publication_date}</p>
-            </div>
+          <form>
+            <div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p className="text-gray-600">Title</p>
+                <p>{title}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p className="text-gray-600">Author</p>
+                <p>{author}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p className="text-gray-600">Genre</p>
+                <p>{genre}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p className="text-gray-600">Publication Date</p>
+                <p>{publication_date}</p>
+              </div>
 
-            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
-                type="text"
-                placeholder="Input your Review"
-              />
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  placeholder="Input your Review"
+                />
 
-              <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Submit Review
-              </button>
-            </div>
-            <div className="md:grid md:grid-cols-3 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-              <button
-                onClick={() => handleEdit(id)}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Edit
-              </button>
+                <button
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Submit Review
+                </button>
+              </div>
+              <div className="md:grid md:grid-cols-3 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <button
+                  onClick={() => handleEdit(id)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Edit
+                </button>
 
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Delete
-              </button>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Add Book
-              </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Delete
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Add Book
+                </button>
+              </div>
+              <div className="md:grid md:grid-cols-1 m-2 py-2 px-4 rounded bg-cyan-500 hover:bg-cyan-600">
+                Reviews:
+              </div>
             </div>
-            <div className="md:grid md:grid-cols-1 m-2 py-2 px-4 rounded bg-cyan-500 hover:bg-cyan-600">
-              Reviews:
-            </div>
-          </div>
+          </form>
         </div>
       </div>
 
